981277b4e811f825910457d36fd67209d1dfd3b7
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2018-07-09 17:02:11 +0700

    Add files via upload

---
+++ JS/5914210004/JQ_RY/03/01.php
@@ -0,0 +1,4 @@
+<?php
+$text1_1 = md5($_REQUEST['text1']);
+
+echo json_encode($text1_1);
---
+++ JS/5914210004/JQ_RY/itec1208.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Community v8.82 
+MySQL - 5.5.9 : Database - itec1208
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`itec1208` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `itec1208`;
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `student_id` int(3) NOT NULL AUTO_INCREMENT,
+  `student_fname` varchar(50) NOT NULL,
+  `student_lname` varchar(50) NOT NULL,
+  `student_user` varchar(50) NOT NULL,
+  `student_pass` char(32) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`student_id`,`student_fname`,`student_lname`,`student_user`,`student_pass`) values (1,'Chainarong','Detsupha','uuuu','uuuu'),(2,'dsf','safsadf','asdfsaf','asdf');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
